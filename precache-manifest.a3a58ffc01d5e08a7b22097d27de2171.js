15fcf266ebf91f9ef3f72e9c362582d62e0eaec9
EzequielSaraniti <eze.rc177@gmail.com>
2022-05-27 18:58:47 -0300

    Updates

---
+++ precache-manifest.a3a58ffc01d5e08a7b22097d27de2171.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d99117a029c6923d5f7",
+    "url": "/E-Commerce-React/static/css/main.83c603af.chunk.css"
+  },
+  {
+    "revision": "9d99117a029c6923d5f7",
+    "url": "/E-Commerce-React/static/js/main.9d99117a.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/E-Commerce-React/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "fad26431eef73d1a9aa0",
+    "url": "/E-Commerce-React/static/css/2.625b37d7.chunk.css"
+  },
+  {
+    "revision": "fad26431eef73d1a9aa0",
+    "url": "/E-Commerce-React/static/js/2.fad26431.chunk.js"
+  },
+  {
+    "revision": "9fba3e67083db82372e5",
+    "url": "/E-Commerce-React/static/js/runtime~main.9fba3e67.js"
+  },
+  {
+    "revision": "f27cab39cd5805cc1ed6cac918615cdb",
+    "url": "/E-Commerce-React/static/media/logoHakunix.f27cab39.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/E-Commerce-React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "bf19a17509e4b0b759de35c1d1849fcc",
+    "url": "/E-Commerce-React/index.html"
+  }
+];
