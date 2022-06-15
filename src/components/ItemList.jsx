c85f6636e9d052c46c0097d606536ddaffb9611e
EzequielSaraniti <eze.rc177@gmail.com>
2022-06-15 14:22:23 -0300

    Sincronizar counter

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -70,8 +70,15 @@ export default function ItemList({category}) {
 
     return (
         <>
+            {loading && 
+            <div className='itemListLoading'>
+                <svg className='loading' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                    <path opacity=".25" d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"></path>
+                    <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z">
+                    <animateTransform attributeName="transform" type="rotate" from="0 16 16" to="360 16 16" dur="0.8s" repeatCount="indefinite"></animateTransform>
+                    </path></svg>
+            </div>}
 
-            <div>{loading && "Cargando productos..."}</div>
             <div>{error && "Tienes un error..."}</div>
 
             
